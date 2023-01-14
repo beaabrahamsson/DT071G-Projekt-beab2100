63e10412234e61e3a24c5e81ed8458f445d6774f
Beatrice Abrahamsson <beaabrahamsson@hotmail.com>
2023-01-14 12:31:35 +0100

    github link

--- MindfulNote/Models/About.cs
+++ MindfulNote/Models/About.cs
@@ -10,7 +10,7 @@ namespace MindfulNote.Models
     {
         public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://github.com/beaabrahamsson/DT071G-Projekt-beaab2100";
+        public string MoreInfoUrl => "https://github.com/beaabrahamsson/DT071G-Projekt-beab2100";
         public string Message => "Appen MindfulNote är en applikation skapad för kursen Programmering i C#.NET. Syftet med applikationen är att användaren ska kunna skapa och spara anteckningar. Appen är skapad med XAML och C# med .NET MAUI. För mer information samt källkod, se länk nedan.";
     }
 }
